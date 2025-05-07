ab1f2fbc9ff7cf1d8e8a29ca6f4ca51ed9665f54
seungwoo1209 <106728184+seungwoo1209@users.noreply.github.com>
2025-05-07 12:43:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 113960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,47 +1,22 @@
+from collections import deque
 import sys
-import bisect
 
+MAX = 100_000
+N, K = map(int, sys.stdin.readline().split())
+if N >= K:
+    print(N - K)
+    sys.exit(0)
 
-def main():
-    N, K = map(int, sys.stdin.readline().split())
-    if N > K:
-        print(N - K)
-        sys.exit(0)
-    if N == K:
-        print(0)
-        sys.exit(0)
-    location_arr = []
-    location_arr.append(set([N]))
-    location_arr.append(set())
-    idx = 0
-    answer_candidate = 100000
-    while True:
-        # print(idx)
-        if K in location_arr[idx % 2]:
-            if idx < answer_candidate:
-                answer_candidate = idx
-            break
-        if idx > answer_candidate:
-            break
-        location_arr[(idx + 1) % 2] = set()
-        for location in location_arr[idx % 2]:
-            if location > K:
-                if (idx + location - K) < answer_candidate:
-                    answer_candidate = idx + location - K
-                continue
-            if location > (K / 2 + K / 4):
-                if (idx + K - location) < answer_candidate:
-                    answer_candidate = idx + K - location
-                continue
-            if location + 1 <= 100_000 and location + 1 not in location_arr[idx % 2]:
-                location_arr[(idx + 1) % 2].add(location + 1)
-            if location - 1 >= 0 and location - 1 not in location_arr[idx % 2]:
-                location_arr[(idx + 1) % 2].add(location - 1)
-            if location * 2 <= 100_000 and location * 2 not in location_arr[idx % 2]:
-                location_arr[(idx + 1) % 2].add(location * 2)
-        idx += 1
-    print(answer_candidate)
+visited = [False] * (MAX + 1)
+dq = deque([(N, 0)])
+visited[N] = True
 
-
-if __name__ == '__main__':
-    main()
+while dq:
+    x, t = dq.popleft()
+    if x == K:
+        print(t)
+        break
+    for nx in (x-1, x+1, x*2):
+        if 0 <= nx <= MAX and not visited[nx]:
+            visited[nx] = True
+            dq.append((nx, t+1))
